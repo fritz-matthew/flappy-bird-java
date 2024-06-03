484f568e73f2dafd726a5eb814149a2f98c641b2
fritz-matthew <144710830+fritz-matthew@users.noreply.github.com>
2024-06-03 15:39:26 +0000

    Done

--- FlappyBird.java
+++ FlappyBird.java
@@ -17,8 +17,8 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     //bird class
     int birdX = boardWidth/8;
     int birdY = boardWidth/2;
-    int birdWidth = 34;
-    int birdHeight = 24;
+    int birdWidth = 120;
+    int birdHeight = 40;
 
     class Bird {
         int x = birdX;
@@ -53,7 +53,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
     //game logic
     Bird bird;
-    int velocityX = -4; //move pipes to the left speed (simulates bird moving right)
+    int velocityX = -10; //move pipes to the left speed (simulates bird moving right)
     int velocityY = 0; //move bird up/down speed.
     int gravity = 1;
 
@@ -72,8 +72,8 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         addKeyListener(this);
 
         //load images
-        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
-        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.gif")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.gif")).getImage();
         topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
 
